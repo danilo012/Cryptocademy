b5d902c78ab225149a9c3808bec8630e230f2622
Narottam04 <coutkunal@gmail.com>
2022-04-10 14:05:11 +0530

    performance optimization 2: code spliting with react lazy and suspense

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,45 @@
+import React from 'react';
 import './App.css';
-import {BrowserRouter ,Route,Routes} from 'react-router-dom'
-import Login from './Pages/Login';
 import NotFound404 from './Pages/NotFound404';
-import Signup from './Pages/Signup';
-import ResetPassword from './Pages/ResetPassword';
-import CoinMarket from './Pages/CoinMarket';
-import News from './Pages/News';
 import ProtectedRoute from './Components/ProtectedRoute'
-import ForgotPassword from './Pages/ForgotPassword';
 import ScrollToTop from './Components/ScrollToTop';
-import CurrencyDetailsPage from './Pages/CurrencyDetailsPage';
-import Watchlist from './Pages/Watchlist';
-import Portfolio from './Pages/Portfolio';
-import Dashboard from './Pages/Dashboard';
-import UserProfile from './Pages/UserProfile';
-import Search from './Pages/Search';
-import AiPredections from './Pages/AiPredections';
-import Leaderboard from './Pages/Leaderboard';
-import MoreMobileNavPage from './Pages/MoreMobileNavPage';
-import GlobalStats from './Pages/GlobalStats';
-import VirtualUsdPage from './Pages/VirtualUsdPage';
+import {BrowserRouter ,Route,Routes} from 'react-router-dom'
+import Loader from './Components/Loader';
+const Login = React.lazy(() => import('./Pages/Login'))
+const Signup = React.lazy(() => import('./Pages/Signup'))
+const ResetPassword = React.lazy(() => import('./Pages/ResetPassword'))
+const CoinMarket = React.lazy(() => import('./Pages/CoinMarket'))
+const News = React.lazy(() => import('./Pages/News'))
+const ForgotPassword = React.lazy(() => import('./Pages/ForgotPassword'))
+const CurrencyDetailsPage = React.lazy(() => import('./Pages/CurrencyDetailsPage'))
+const Watchlist = React.lazy(() => import('./Pages/Watchlist'))
+const Portfolio = React.lazy(() => import('./Pages/Portfolio'))
+const Dashboard = React.lazy(() => import('./Pages/Dashboard'))
+const UserProfile = React.lazy(() => import('./Pages/UserProfile'))
+const Search = React.lazy(() => import('./Pages/Search'))
+const AiPredections = React.lazy(() => import('./Pages/AiPredections'))
+const Leaderboard = React.lazy(() => import('./Pages/Leaderboard'))
+const MoreMobileNavPage = React.lazy(() => import('./Pages/MoreMobileNavPage'))
+const GlobalStats = React.lazy(() => import('./Pages/GlobalStats'))
+const VirtualUsdPage = React.lazy(() => import('./Pages/VirtualUsdPage'))
+
+// import Login from './Pages/Login';
+// import Signup from './Pages/Signup';
+// import ResetPassword from './Pages/ResetPassword';
+// import CoinMarket from './Pages/CoinMarket';
+// import News from './Pages/News';
+// import ForgotPassword from './Pages/ForgotPassword';
+// import CurrencyDetailsPage from './Pages/CurrencyDetailsPage';
+// import Watchlist from './Pages/Watchlist';
+// import Portfolio from './Pages/Portfolio';
+// import Dashboard from './Pages/Dashboard';
+// import UserProfile from './Pages/UserProfile';
+// import Search from './Pages/Search';
+// import AiPredections from './Pages/AiPredections';
+// import Leaderboard from './Pages/Leaderboard';
+// import MoreMobileNavPage from './Pages/MoreMobileNavPage';
+// import GlobalStats from './Pages/GlobalStats';
+// import VirtualUsdPage from './Pages/VirtualUsdPage';
 
 
 function App() {
@@ -27,115 +47,110 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <ScrollToTop/>
-        <Routes>
-          <Route path = "/" element={<Login/>} />
-
-          {/* <Route path = "/app" element={<Dashboard/>} />
-          <Route path = "/app/market" element={<CoinMarket/>} />
-          <Route path = "/app/search" element={<Search/>} />
-          <Route path = "/app/ai" element={<AiPredections/>} />
-          <Route path = "/app/coins/:id" element={<CurrencyDetailsPage/>} />
-          <Route path = "/app/news" element={<News/>} />
-          <Route path = "/app/watchlist" element={<Watchlist/>} />
-          <Route path = "/app/portfolio" element={<Portfolio/>} />
-          <Route path = "/app/profile" element={<UserProfile/>} /> */}
-          <Route path = "/app/coins/:id" element={<CurrencyDetailsPage/>} />
-          <Route path = "/app" 
-            element={
-              <ProtectedRoute>
-                <Dashboard/>
-              </ProtectedRoute>
-            } 
-          />
-          <Route path = "/app/market" 
-            element={
-              <ProtectedRoute>
-                <CoinMarket/>
-              </ProtectedRoute>
-            } 
-          />
-          <Route path = "/app/search" 
-            element={
-              <ProtectedRoute>
-                <Search/>
-              </ProtectedRoute>
-            } 
-          />
-          <Route path = "/app/leaderboard" 
-            element={
-              <ProtectedRoute>
-                <Leaderboard/>
-              </ProtectedRoute>
-            } 
-          />
-          <Route path = "/app/ai" 
-            element={
-              <ProtectedRoute>
-                <AiPredections/>
-              </ProtectedRoute>
-            } 
-          />
-          <Route path = "/app/coin/USD" 
-            element={
-              <ProtectedRoute>
-                <VirtualUsdPage/>
-              </ProtectedRoute>
-            } 
-          />
-          <Route path = "/app/coin/:id" 
-            element={
-              <ProtectedRoute>
-                <CurrencyDetailsPage/>
-              </ProtectedRoute>
-            } 
-          />
-          <Route path = "/app/news" 
-            element={
-              <ProtectedRoute>
-                <News/>
-              </ProtectedRoute>
-            } 
-          />
-          <Route path = "/app/watchlist" 
-            element={
-              <ProtectedRoute>
-                <Watchlist/>
-              </ProtectedRoute>
-            } 
-          />
-          <Route path = "/app/portfolio" 
-            element={
-              <ProtectedRoute>
-                <Portfolio/>
-              </ProtectedRoute>
-            } 
-          />
-          <Route path = "/app/profile" 
-            element={
-              <ProtectedRoute>
-                <UserProfile/>
-              </ProtectedRoute>
-            } 
-          />
-          <Route path = "/app/more" 
-            element={
-              <ProtectedRoute>
-                <MoreMobileNavPage/>
-              </ProtectedRoute>
-            } 
-          />
-          <Route path = "/app/market/globalStats" 
-            element={
-              <ProtectedRoute>
-                <GlobalStats/>
-              </ProtectedRoute>
-            } 
-          />
-          <Route path = "/signup" element={<Signup/>} />
-          <Route path = "/resetPassword" element={<ResetPassword/>} />
-          <Route path = "/forgotPassword" element={<ForgotPassword/>} />
-          <Route path = "*" element={<NotFound404/>} />
-        </Routes>
+        <React.Suspense fallback={
+          <div className='w-screen h-screen bg-black'>
+            <Loader/>
+          </div>
+        }>
+          <Routes>
+              <Route path = "/" element={<Login/>} />
+              <Route path = "/app" 
+                element={
+                  <ProtectedRoute>
+                    <Dashboard/>
+                  </ProtectedRoute>
+                } 
+              />
+              <Route path = "/app/market" 
+                element={
+                  <ProtectedRoute>
+                    <CoinMarket/>
+                  </ProtectedRoute>
+                } 
+              />
+              <Route path = "/app/search" 
+                element={
+                  <ProtectedRoute>
+                    <Search/>
+                  </ProtectedRoute>
+                } 
+              />
+              <Route path = "/app/leaderboard" 
+                element={
+                  <ProtectedRoute>
+                    <Leaderboard/>
+                  </ProtectedRoute>
+                } 
+              />
+              <Route path = "/app/ai" 
+                element={
+                  <ProtectedRoute>
+                    <AiPredections/>
+                  </ProtectedRoute>
+                } 
+              />
+              <Route path = "/app/coin/USD" 
+                element={
+                  <ProtectedRoute>
+                    <VirtualUsdPage/>
+                  </ProtectedRoute>
+                } 
+              />
+              <Route path = "/app/coin/:id" 
+                element={
+                  <ProtectedRoute>
+                    <CurrencyDetailsPage/>
+                  </ProtectedRoute>
+                } 
+              />
+              <Route path = "/app/news" 
+                element={
+                  <ProtectedRoute>
+                    <News/>
+                  </ProtectedRoute>
+                } 
+              />
+              <Route path = "/app/watchlist" 
+                element={
+                  <ProtectedRoute>
+                    <Watchlist/>
+                  </ProtectedRoute>
+                } 
+              />
+              <Route path = "/app/portfolio" 
+                element={
+                  <ProtectedRoute>
+                    <Portfolio/>
+                  </ProtectedRoute>
+                } 
+              />
+              <Route path = "/app/profile" 
+                element={
+                  <ProtectedRoute>
+                    <UserProfile/>
+                  </ProtectedRoute>
+                } 
+              />
+              <Route path = "/app/more" 
+                element={
+                  <ProtectedRoute>
+                    <MoreMobileNavPage/>
+                  </ProtectedRoute>
+                } 
+              />
+              <Route path = "/app/market/globalStats" 
+                element={
+                  <ProtectedRoute>
+                    <GlobalStats/>
+                  </ProtectedRoute>
+                } 
+              />
+              <Route path = "/signup" element={<Signup/>} />
+              <Route path = "/resetPassword" element={<ResetPassword/>} />
+              <Route path = "/forgotPassword" element={<ForgotPassword/>} />
+              <Route path = "*" element={<NotFound404/>} />
+          </Routes>
+        </React.Suspense>
       </BrowserRouter>
     </div>
   );
