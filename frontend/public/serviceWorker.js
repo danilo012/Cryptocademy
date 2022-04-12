6938c22b97a7f86efe69ad598ecc120716a865b6
Narottam04 <coutkunal@gmail.com>
2022-04-12 18:50:35 +0530

    offline page implementation try 3

--- frontend/public/serviceWorker.js
+++ frontend/public/serviceWorker.js
@@ -1,66 +1,50 @@
 const CACHE_NAME = "cryptocademy-v1";
-const urlsToCache = 'offline.html';
+const urlsToCache = ['offline.html'];
 
-const self = this;
+addEventListener('install', (event) => {
+    event.waitUntil(async function() {
+        const cache = await caches.open(CACHE_NAME);
+        await cache.addAll(urlsToCache);
+    }());
+});
+  
+// See https://developers.google.com/web/updates/2017/02/navigation-preload#activating_navigation_preload
+addEventListener('activate', event => {
+    event.waitUntil(async function() {
+        // Feature-detect
+        if (self.registration.navigationPreload) {
+            // Enable navigation preloads!
+            await self.registration.navigationPreload.enable();
+            console.log("navigationPreload Enabled")
+        }
+    }());
+});
+  
+addEventListener('fetch', (event) => {
+    const { request } = event;
 
-// install sw
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open(CACHE_NAME)
-        .then((cache) => {
-            console.log('opened cache')
+    // Always bypass for range requests, due to browser bugs
+    if (request.headers.has('range')) return;
+    event.respondWith(async function() {
+        // Try to get from the cache:
+        const cachedResponse = await caches.match(request);
+        if (cachedResponse) return cachedResponse;
 
-            // return cache.addAll(urlsToCache);
-            return cache.add(new Request(urlsToCache, { cache: "reload" }))
-        })
-    )
-})
+        try {
+            // See https://developers.google.com/web/updates/2017/02/navigation-preload#using_the_preloaded_response
+            const response = await event.preloadResponse;
+            if (response) return response;
 
-// self.addEventListener("install", event => {
-//     this.skipWaiting();
-//     event.waitUntil(
-//         caches.open(CACHE_NAME)
-//         .then(cache => {
-//             return fetch('/offline')
-//             .then(response => cache.put('/offline', new Response(response.body)));
-//         })
-//     )
-// });
-
-// listen  for requests
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request)
-        .then(() => {
-            return fetch(e.request)
-            .catch(() => caches.match('offline.html'))
-        })
-
-    )
-})
-// activate the sw
-self.addEventListener('activate', (e) => {
-    
-    if ("navigationPreload" in self.registration) {
-        self.registration.navigationPreload.enable()
-        .then(() => {
-            console.log("navigationPreload is enabled")
-        });
-    }  
-    // Tell the active service worker to take control of the page immediately.
-    self.clients.claim();
-    
-    const cacheWhiteList = [];
-    cacheWhiteList.push(CACHE_NAME);
-
-    e.waitUntil(
-        caches.keys()
-        .then((cacheNames) => Promise.all(
-            cacheNames.map((cacheName) => {
-                if(!cacheWhiteList.includes(cacheName)) {
-                    return caches.delete(cacheName)
-                }
-            })
-        ))
-    )
-})
+            // Otherwise, get from the network
+            return await fetch(request);
+            
+        } catch (err) {
+            // If this was a navigation, show the offline page:
+            if (request.mode === 'navigate') {
+                return caches.match('offline.html');
+            }
+            // Otherwise throw
+            throw err;
+        }
+    }());
+});
