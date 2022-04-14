171e667dcafc88a879b9af8f82b61c9bc320d319
Narottam04 <coutkunal@gmail.com>
2022-04-14 15:46:13 +0530

    offline fallback page is now working

--- frontend/public/serviceWorker.js
+++ frontend/public/serviceWorker.js
@@ -1,16 +1,3 @@
-/*
-Copyright 2015, 2019 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
-
 // Incrementing OFFLINE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 const OFFLINE_VERSION = 1.1;
@@ -29,18 +16,18 @@ self.addEventListener('install', (event) => {
   })());
 });
 
-// self.addEventListener('activate', (event) => {
-//   event.waitUntil((async () => {
-//     // Enable navigation preload if it's supported.
-//     // See https://developers.google.com/web/updates/2017/02/navigation-preload
-//     if ('navigationPreload' in self.registration) {
-//       await self.registration.navigationPreload.enable();
-//     }
-//   })());
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    // Enable navigation preload if it's supported.
+    // See https://developers.google.com/web/updates/2017/02/navigation-preload
+    if ('navigationPreload' in self.registration) {
+      await self.registration.navigationPreload.enable();
+    }
+  })());
 
-//   // Tell the active service worker to take control of the page immediately.
-//   self.clients.claim();
-// });
+  // Tell the active service worker to take control of the page immediately.
+  self.clients.claim();
+});
 
 self.addEventListener('fetch', (event) => {
   // We only want to call event.respondWith() if this is a navigation request
