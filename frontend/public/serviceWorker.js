8e0904e743c01039e051ac933dd432b4aba880b6
Narottam04 <coutkunal@gmail.com>
2022-04-14 13:45:24 +0530

    removed activate listener in service worker

--- frontend/public/serviceWorker.js
+++ frontend/public/serviceWorker.js
@@ -29,18 +29,18 @@ self.addEventListener('install', (event) => {
   })());
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil((async () => {
-    // Enable navigation preload if it's supported.
-    // See https://developers.google.com/web/updates/2017/02/navigation-preload
-    if ('navigationPreload' in self.registration) {
-      await self.registration.navigationPreload.enable();
-    }
-  })());
+// self.addEventListener('activate', (event) => {
+//   event.waitUntil((async () => {
+//     // Enable navigation preload if it's supported.
+//     // See https://developers.google.com/web/updates/2017/02/navigation-preload
+//     if ('navigationPreload' in self.registration) {
+//       await self.registration.navigationPreload.enable();
+//     }
+//   })());
 
-  // Tell the active service worker to take control of the page immediately.
-  self.clients.claim();
-});
+//   // Tell the active service worker to take control of the page immediately.
+//   self.clients.claim();
+// });
 
 self.addEventListener('fetch', (event) => {
   // We only want to call event.respondWith() if this is a navigation request
