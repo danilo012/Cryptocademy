49cd883393b449708c68972aa5c401058769f4b6
Narottam04 <coutkunal@gmail.com>
2022-06-10 12:09:04 +0530

    removed light mode styling from reset virtual balance

--- frontend/src/Components/ResetVirtualBalance.js
+++ frontend/src/Components/ResetVirtualBalance.js
@@ -50,10 +50,10 @@ const ResetVirtualBalance = ({modal,setModal}) => {
         <div id="defaultModal" tabindex="-1" aria-hidden="true" className={`${!modal && "hidden" } flex flex-col overflow-y-auto overflow-x-hidden fixed right-0 left-0  z-50 justify-center items-center md:inset-0 h-modal sm:h-full`} >
             <div className="relative p-4 w-full max-w-2xl h-full md:h-auto">
                 {/* <!-- Modal content --> */}
-                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <div className="relative rounded-lg shadow bg-gray-700">
                     {/* <!-- Modal header --> */}
-                    <div className="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
-                        <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                    <div className="flex justify-between items-start p-4 rounded-t border-gray-600">
+                        <h3 className="text-xl font-semibold text-white">
                             Reset Virtual USD balance
                         </h3>
                         <button type="button" className="text-gray-400 bg-transparent  rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-gray-600 hover:text-white" data-modal-toggle="large-modal" 
@@ -63,15 +63,15 @@ const ResetVirtualBalance = ({modal,setModal}) => {
                     </div>
                     {/* <!-- Modal body --> */}
                     <div className="p-6 space-y-6">
-                        <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                        <p className="text-base leading-relaxed text-gray-400">
                             You can use the following action to delete your portfolio account and restore your initial USD balance of 100k to the account.
                         </p>
                     </div>
                     {/* <!-- Modal footer --> */}
-                    <div className="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
+                    <div className="flex items-center p-6 space-x-2 rounded-b border-t border-gray-600">
                         <button data-modal-toggle="defaultModal"
                         onClick={resetVirtualBalanceHandler}
-                        disabled={resetLoading} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        disabled={resetLoading} type="button" className="text-white bg-blue-700  focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">
                             {
                                 resetLoading ? `Resetting Account...`
                                 :`Reset Account`
