b7e91893b6dc4e32bac97bc2a0f5de66f54ec2a8
Narottam <coutkunal@gmail.com>
2023-01-26 08:14:03 +0530

    mobile styling consistency check

--- frontend/src/Pages/Watchlist.jsx
+++ frontend/src/Pages/Watchlist.jsx
@@ -92,7 +92,9 @@ const Watchlist = () => {
 
   return (
     <section className="lg:px-4 py-2 lg:py-8  max-w-[1600px]">
-      <p className="text-white font-bold text-2xl md:text-3xl font-title mb-4 ml-3">WatchList</p>
+      <p className="text-white font-bold text-2xl md:text-3xl font-title mt-4 lg:mt-0 mb-4 ml-3">
+        WatchList
+      </p>
       <p className="text-white font-semibold text-md font-title  ml-3">
         Swipe left to delete or view the coins.
       </p>
