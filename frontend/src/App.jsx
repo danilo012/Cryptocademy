bdff8a62473e407a06e1e197237e447d025c4ed3
Narottam <coutkunal@gmail.com>
2023-02-02 07:55:31 +0530

    fixed loading logic in currency details and coinsTable page, removed capcitor library for web paltform

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
-import { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
-import { PushNotifications } from "@capacitor/push-notifications";
 
 import "./App.css";
 
@@ -8,28 +6,6 @@ import AnimatedRoutes from "./Components/AnimatedRoutes";
 import ScrollToTop from "./Components/ScrollToTop";
 
 function App() {
-  useEffect(() => {
-    const registerNotifications = async () => {
-      try {
-        let permStatus = await PushNotifications.checkPermissions();
-
-        if (permStatus.receive === "prompt") {
-          permStatus = await PushNotifications.requestPermissions();
-        }
-
-        if (permStatus.receive !== "granted") {
-          console.log("User denied permissions!");
-          return;
-        }
-
-        await PushNotifications.register();
-      } catch (error) {
-        console.log("push notification Error: ", error);
-      }
-    };
-    registerNotifications();
-  }, []);
-
   return (
     <div className="App scrollbar scrollbar-thumb-gray-900 scrollbar-track-gray-100 bg-black">
       <BrowserRouter>
